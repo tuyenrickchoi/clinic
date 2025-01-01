8868a87a0e4826128a8263ca7a9f554b7068d8ac
Mai Thiệu Tín <160812853+WilliamSimon13@users.noreply.github.com>
2025-01-01 19:18:39 +0700

    implements service-detail pages

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import TopBar from './components/scripts/top-bar';
 import Footer from './components/scripts/footer';
 import HeroBanner from './components/scripts/herobanner';
 import Gallery from './views/gallery';
-import GalleryDetail from './components/scripts/GalleryDetail'; // Import trang chi tiết Gallery
+import GalleryDetail from './views/GalleryDetail'; // Import trang chi tiết Gallery
 import About from './components/scripts/AboutUs';  // Trang AboutUs
 import Contact from './components/scripts/Contact'; // Trang Contact
 import SkinLaser from './components/scripts/SkinLaserPage';
 import Services from './views/service'; // Thêm import cho Services component
+import ServiceDetail from './views/serviceDetail';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; // Sử dụng Switch thay Routes
 
 class App extends React.Component {
@@ -24,6 +25,7 @@ class App extends React.Component {
             <Route path="/SkinLaserPage" component={SkinLaser} />
             <Route path="/contact" component={Contact} />
             <Route path="/services" component={Services} />
+            <Route path="/service-detail/:id" component={ServiceDetail} />
           </Switch>
           <Footer />
         </div>
