75a797b26830f60dc8a53a363094691d2511cc22
hongngocdoan <hongngoc2449@gmail.com>
2025-01-02 19:00:04 +0700

    fix delete order in admin

--- src/controllers/sellProduct.controller.js
+++ src/controllers/sellProduct.controller.js
@@ -1,20 +1,34 @@
 const SellProduct = require('../models/sellProduct.models');
 
-exports.createSellProduct = async (req, res) => {
-  try {
-    const sellProduct = new SellProduct(req.body);
-    await sellProduct.save();
-    res.status(201).send(sellProduct);
-  } catch (error) {
-    res.status(400).send({ error: error.message });
-  }
-};
+module.exports = {
+    createSellProduct: async (req, res) => {
+        try {
+            const sellProduct = new SellProduct(req.body);
+            await sellProduct.save();
+            res.status(201).send(sellProduct);
+        } catch (error) {
+            res.status(400).send({ error: error.message });
+        }
+    },
 
-exports.getAllSellProducts = async (req, res) => {
-  try {
-    const sellProducts = await SellProduct.find().sort({ createdAt: -1 });
-    res.status(200).json(sellProducts);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+    getAllSellProducts: async (req, res) => {
+        try {
+            const sellProducts = await SellProduct.find().sort({ createdAt: -1 });
+            res.status(200).json(sellProducts);
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    },
+
+    deleteSellProduct: async (req, res) => {
+        try {
+            const sellProduct = await SellProduct.findByIdAndDelete(req.params.id);
+            if (!sellProduct) {
+                res.status(404).json({ message: 'Sell product not found' });
+            }
+            res.status(200).json(sellProduct);
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    }
 };
--- src/routers/sellProduct.router.js
+++ src/routers/sellProduct.router.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const {
     createSellProduct,
-    getAllSellProducts
+    getAllSellProducts,
+    deleteSellProduct
 } = require('../controllers/sellProduct.controller');
 
 router.post('/', createSellProduct);
 router.get('/', getAllSellProducts);
+router.delete('/:id', deleteSellProduct); 
 
 module.exports = router;
