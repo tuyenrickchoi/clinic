521b809733e06b83a3d9e6170be2c97b9ee2c402
Mai Thiệu Tín <160812853+WilliamSimon13@users.noreply.github.com>
2025-01-02 15:51:13 +0700

    fix ui homepage

--- src/components/scripts/massage.js
+++ src/components/scripts/massage.js
@@ -4,52 +4,52 @@ import border from '../../assets/border.png';
 import massage1 from '../../assets/massage.jpg';
 import massage2 from '../../assets/massage1.jpg';
 import massage3 from '../../assets/massage2.jpg';
-import Footer from './footer';
+import { useHistory } from 'react-router-dom'; // Thêm useHistory để điều hướng
 
-class Massage extends React.Component{
-  render(){
-      return(
-          <div className="Massage">
-              <div className="container">
-                  <div className="row">
-                  <div className="col-sm-12 col-md-12 col-lg-12 col-xl-12 text-center">
-                     <h2 className="text-white massage-heading">MASSAAGE TREATMENTS</h2>
-                     <div className="divider-img">
-                     <img src={border}></img>
-                     </div>
-                    </div>
-                  </div>
-                  <div class="row  mt-3">
-                    <div className="col-sm-12 col-md-4 col-lg-4 col-xl-4 massage-content">
-                    <h4 className="text-white massage-heading">Facial Massage</h4>
-                    <div className="message-img">
-                    <img src={massage1} />
-                    </div>
-                        <p className="text-white">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
-                    </div>
-                    <div className="col-sm-12 col-md-4 col-lg-4 col-xl-4 massage-content">
-                    <h4 className="text-white massage-heading">Relaxing Massage</h4>
-                    <div className="message-img">
-                    <img src={massage2} />
-                    </div>
-                    <p className="text-white">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
-                    </div>
-                    <div className="col-sm-12 col-md-4 col-lg-4 col-xl-4 massage-content">
-                    <h4 className="text-white massage-heading">Deep Tissue Massage</h4>
-                    <div className="message-img">
-                    <img src={massage3} />
-                    </div>
-                    <p className="text-white">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
-                    </div>
-                
-                  </div>
-              </div>
-             
+const Massage = () => {
+  const history = useHistory(); // Khai báo hook useHistory
+
+  const handleServiceClick = (service) => {
+    history.push(`/services?service=${service}`); // Chuyển hướng tới trang dịch vụ với query parameter
+  };
+
+  return (
+    <div className="Massage">
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12 col-md-12 col-lg-12 col-xl-12 text-center">
+            <h2 className="text-white massage-heading">MASSAGE TREATMENTS</h2>
+            <div className="divider-img">
+              <img src={border} alt="" />
+            </div>
           </div>
-
-      );
-  }
-}
-
+        </div>
+        <div className="row mt-3">
+          <div className="col-sm-12 col-md-4 col-lg-4 col-xl-4 massage-content">
+            <h4 className="text-white massage-heading">Facial Massage</h4>
+            <div className="message-img" onClick={() => handleServiceClick('Massage')}>
+              <img src={massage1} alt="" />
+            </div>
+            <p className="text-white">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+          </div>
+          <div className="col-sm-12 col-md-4 col-lg-4 col-xl-4 massage-content">
+            <h4 className="text-white massage-heading">Relaxing Massage</h4>
+            <div className="message-img" onClick={() => handleServiceClick('Massage')}>
+              <img src={massage2} alt="" />
+            </div>
+            <p className="text-white">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+          </div>
+          <div className="col-sm-12 col-md-4 col-lg-4 col-xl-4 massage-content">
+            <h4 className="text-white massage-heading">Deep Tissue Massage</h4>
+            <div className="message-img" onClick={() => handleServiceClick('Massage')}>
+              <img src={massage3} alt="" />
+            </div>
+            <p className="text-white">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Massage;
